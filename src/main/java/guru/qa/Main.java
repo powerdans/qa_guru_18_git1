2e818d551711a3a94fb680b569fbbd6b548dc288
powerdans <77230160+powerdans@users.noreply.github.com>
2023-10-28 16:52:44 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[]!!!!!!!!!! args) {
 
         System.out.println(getmassege());
     }
